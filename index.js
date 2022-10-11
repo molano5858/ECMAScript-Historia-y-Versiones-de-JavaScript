6493dd3f8148da8c0a069950c00c2aee041db075
Felipe Molano <molano5858@gmail.com>
2022-10-11 11:37:49 -0500

    Init de npm, creacion del index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+//ECMA
+// Europena Computer Manufacturer Association
+//ECMAScript  es el conjunto de standares y reglas  en los que se va a basar la comunidad
+
+//TC39: Es un grupo de hackers, academicos y/o personas afines a la tecnologia que se encargan de las actualizaciones, bajo el mando de ECMA.
